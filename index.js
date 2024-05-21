2089565dcc7292e5993929cfdba2f0f5d282ba36
Behzad Shayegh <behzad@Behzads-MacBook-Pro.local>
2024-05-21 20:35:30 +0200

    Iran trip

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ var travels =
 Montreal,45.5019,-73.5674
 Toronto,43.6502,-79.4232
 Niagara,43.1362,-79.1077
+Nowshahr,36.6494,51.4887
+Masal,37.3633,49.1324
+Gisum,37.6777,49.0518
+Rasht,37.2713,49.5921
+Olsabelangah,37.3101146,48.9604798
 Toronto,43.6732,-79.3632
 Niagara,43.0942,-79.1297
 Jasper,52.8737,-118.0814
@@ -15,6 +20,9 @@ Vienna,48.2081,16.3713
 Zurich,47.3769,8.5417
 Venice,45.4404,12.3160
 Florence,43.7700,11.2577
+Rome,41.8967,12.4822
+Vatican,41.9029,12.4534
+Munich,48.1351,11.5820
 `;
 
 var stays = 
@@ -57,7 +65,7 @@ require([
     // Define a symbol for drawing the point
     var stayMarkerSymbol = {
         type: "simple-marker", // autocasts as new SimpleMarkerSymbol()
-        color: [64, 176, 64], // Green color
+        color: [64, 176, 64, .8], // Green color
         style: "square", // Specifies the marker is square-shaped
         size: "12px", // Larger size, adjust as needed
         outline: {
@@ -67,7 +75,7 @@ require([
     };
     var travelMarkerSymbol = {
         type: "simple-marker", // autocasts as new SimpleMarkerSymbol()
-        color: [196, 64, 64], // Orange color
+        color: [196, 64, 64, .4], // Orange color
         size: "8px", // Larger size, adjust as needed
         outline: {
             color: [255, 255, 255], // White outline color
@@ -75,30 +83,6 @@ require([
         }
         };
       // Parse CSV data
-      const rows1 = travels.split('\n').slice(1); // skip header
-      rows1.forEach(row => {
-        const columns = row.split(',');
-        if (columns.length === 3) {
-          const city = columns[0];
-          const latitude = parseFloat(columns[1]);
-          const longitude = parseFloat(columns[2]);
-
-          // Create a point for each city
-          var point = {
-            type: "point",
-            longitude: longitude,
-            latitude: latitude
-          };
-
-          // Create a graphic for each city
-          var pointGraphic = new Graphic({
-            geometry: point,
-            symbol: travelMarkerSymbol
-          });
-          graphicsLayer.add(pointGraphic);
-        }
-      });
-
       const rows2 = stays.split('\n').slice(1); // skip header
       rows2.forEach(row => {
         const columns = row.split(',');
@@ -118,6 +102,30 @@ require([
           var pointGraphic = new Graphic({
             geometry: point,
             symbol: stayMarkerSymbol
+          });
+          graphicsLayer.add(pointGraphic);
+        }
+      });
+      
+      const rows1 = travels.split('\n').slice(1); // skip header
+      rows1.forEach(row => {
+        const columns = row.split(',');
+        if (columns.length === 3) {
+          const city = columns[0];
+          const latitude = parseFloat(columns[1]);
+          const longitude = parseFloat(columns[2]);
+
+          // Create a point for each city
+          var point = {
+            type: "point",
+            longitude: longitude,
+            latitude: latitude
+          };
+
+          // Create a graphic for each city
+          var pointGraphic = new Graphic({
+            geometry: point,
+            symbol: travelMarkerSymbol
           });
           graphicsLayer.add(pointGraphic);
         }
